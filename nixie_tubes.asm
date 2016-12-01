18ae62613989980fbfb60b69c9276773fd8b14df
Chaoyang Liu <chaoyanglius@gmail.com>
2016-12-01 11:34:07 +0800

    do some changes
    I change symbol "start" into ".startup", the latter is standard. In
    addition, I add some comments.

--- nixie_tubes.asm
+++ nixie_tubes.asm
@@ -6,26 +6,23 @@
 
 .code
 
-start:	lea bx,seg7
-		mov dx,0f003h
-		mov al,81h
-		out dx,al		;设置控制字
-		mov dx,0f001h	;数码管的位控制地址
-		mov al,00h
-		out dx,al		;选择数码管的全部8位
+.startup
+	lea bx,seg7
+	mov dx,0f003h
+	mov al,81h
+	out dx,al	;设置控制字
+	mov dx,0f001h	;数码管的位控制地址
+	mov al,7fh
+	out dx,al	;选择数码管的第1位
 	
 again:	mov dx,0f002h
-		in al,dx
-		and al,0fh
-		mov ah,0
-		mov si,ax
-		mov al,[bx+si]
-		mov dx,0f000h
-		out dx,al
-		jmp again
+	in al,dx
+	and al,0fh
+	mov ah,0
+	mov si,ax
+	mov al,[bx+si]
+	mov dx,0f000h
+	out dx,al
+	jmp again
 
-end start
-	
-	
-	
-	
+end
