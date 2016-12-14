bd57f3ee5b3c156a66a8811fe78e26c036356865
Chaoyang Liu <chaoyanglius@gmail.com>
2016-12-14 15:38:43 +0800

    add some comments

--- 8259_pra.asm
+++ 8259_pra.asm
@@ -4,9 +4,9 @@ IO8259_0	EQU	0F000H
 IO8259_1	EQU	0F001H
 	.STACK      100
 	.DATA    
-BUFFER          DB      8 DUP(?)
-Counter		DB	?
-ReDisplayFlag	DB	0
+BUFFER          DB      8 DUP(?)	;设置缓冲区
+Counter		DB	?					;计数值
+ReDisplayFlag	DB	0				;显示标志位
 	.CODE
 START:  	MOV     AX,@DATA
         	MOV     DS,AX
@@ -18,21 +18,21 @@ START:  	MOV     AX,@DATA
 		MOV	ReDisplayFlag,1	;需要显示
 		STI			;开中断
 START1:		CMP	ReDisplayFlag,0
-		JZ	START1
+		JZ	START1					;检查显示标志位是否为1，即确定是否需要显示
 		CALL	LedDisplay
-		MOV	ReDisplayFlag,0
+		MOV	ReDisplayFlag,0			
 		JMP	START1
 
 Init8259	PROC	NEAR
-		MOV	DX,IO8259_0
-		MOV	AL,13H
+		MOV	DX,IO8259_0				
+		MOV	AL,13H					;设置ICW1的控制字
 		OUT	DX,AL
 		MOV	DX,IO8259_1
-		MOV	AL,08H
+		MOV	AL,08H					;设置ICW2的控制字
 		OUT	DX,AL
-		MOV	AL,09H
+		MOV	AL,09H					;设置ICW4的控制字
 		OUT	DX,AL
-		MOV	AL,0FEH
+		MOV	AL,0FEH					;设置OCW1的控制字
 		OUT	DX,AL
 		RET
 Init8259	ENDP
